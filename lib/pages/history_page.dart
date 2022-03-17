2c6732494e2572ff548ae7d6d383e26db48257e4
Spencer Leisch <leisc020@d.umn.edu>
2022-03-17 16:08:26 -0500

    Added history_page.dart and filled it with a basic page consisting of a 5x7 grid with each box filled with "Day *dayNumber*".

---
+++ lib/pages/history_page.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class HistoryPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: Fix day display so it starts with 1 instead of 0
+    // TODO: Make grid start on the proper day of the week instead of every month starting on sunday
+    // TODO: Display the name of the day in top row
+    // TODO: Change box color to match selected mood that day
+    // TODO: Allow user to click on day to view the activities they did that day
+    const title = "History";
+
+    return MaterialApp(
+      title: title,
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text(title),
+        ),
+        body:GridView.count(
+          crossAxisCount: 7, //sets the grid to be 7 boxes wide
+          // Generate 35 widgets that display their index in the List.
+          children: List.generate(35, (index) {
+            return Center(
+              child: Text(
+                'Day $index',
+                style: Theme.of(context).textTheme.headline5,
+              ),
+            );
+          }),
+        ),
+      ),
+    );
+/*
+    return Scaffold(
+      body: Center(
+        child: Text('Your History Here!'),
+      ),
+    );
+    */
+
+  }
+
+}
