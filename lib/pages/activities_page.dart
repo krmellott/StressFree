64edc0b713f3a0602a430b3d247b007b35cb67a8
Gideon Okoroafor <okoro062@umn.edu>
2022-03-17 15:39:10 -0500

    added a new file for pages

---
+++ lib/pages/activities_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class ActivitiesPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Scaffold(
+      body: Center(
+        child: Text('Activities Page'),
+      ),
+    );
+  }
+  
+}
---
+++ lib/pages/home_page.dart
@@ -0,0 +1,82 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+
+import 'activities_page.dart';
+
+class Home extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        theme: ThemeData(
+          primaryColor: Colors.green,
+        ),
+        home: Builder(
+            builder: (context) => Scaffold(
+              appBar: AppBar(
+                title: Text("StressFree!"),
+              ),
+              body: Center(
+                  child: Column(
+                    children: <Widget>[
+                      Padding(
+                        padding: EdgeInsets.only(top: 20.0, bottom: 20.0),
+                        child: Text("StressFree!",style: const TextStyle(fontWeight: FontWeight.bold, color: Colors.green), textScaleFactor: 4,)
+                        ,),
+                      ElevatedButton(
+                        style: ElevatedButton.styleFrom(
+                            primary: Colors.green
+                        ),
+                        child: Text('Begin'),
+                        onPressed: () {
+                          Navigator.push(context,
+                              MaterialPageRoute(
+                                  builder: (context) => ActivitiesPage()
+                              ));
+                          // Navigator.of(context).push(MaterialPageRoute(builder: (BuildContext context) {
+                          //   return MyApp();
+                          // }
+                          // ));
+                        },
+                      )
+                    ],
+                  )
+              ),
+            )
+        )
+    );
+  }
+}
+
+class _MyApp extends State<MyApp> {
+  final Future<FirebaseApp> _fbApp = Firebase.initializeApp();
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'StressFree!',
+      theme: ThemeData(
+        primaryColor: Colors.green,
+      ),
+      home: FutureBuilder(
+        future: _fbApp,
+        builder: (context, snapshot) {
+          if (snapshot.hasError) {
+            print('Error: ${snapshot.error.toString()}');
+            return Text("Something went wrong!");
+          } else if (snapshot.hasData) {
+            return Text("here"); //change this
+            //return RandomWords();
+          } else {
+            return Center(
+              child: CircularProgressIndicator(),
+            );
+          }
+        },
+      ),
+    );
+  }
+}
+
+class MyApp extends StatefulWidget {
+  @override
+  _MyApp createState() => _MyApp();
+}
