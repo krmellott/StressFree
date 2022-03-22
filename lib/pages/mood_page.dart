e9f5e5f32b484568fe175fa361c6d06b88e75b98
Kyle Mellott <mello077@d.umn.edu>
2022-03-22 16:19:17 -0500

    Added a dropdown button and resolved errors

--- lib/pages/mood_page.dart
+++ lib/pages/mood_page.dart
@@ -1,5 +1,7 @@
-import '/utils/units_constant.dart';
-
+import 'package:firstapp/utils/units_constant.dart';
+import 'package:flutter/src/material/dropdown.dart';
+import 'package:firstapp/controller/stressFree_Controller.dart';
+import 'package:firstapp/model/stressFree_Model.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
@@ -11,6 +13,9 @@ class MoodPage extends StatefulWidget {
 class _MoodPage extends State<MoodPage> {
   @override
   Moods currMood = Moods.Neutral;
+  final controllerReference = new stressFree_Controller();
+  final modelReference = new stressFree_Model();
+  DateTime _selectedDate = DateTime.now();
 
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -24,17 +29,42 @@ class _MoodPage extends State<MoodPage> {
                   title: Text("Moods"),
                 ),
                 body: Center(
-                  child: Column(children: <Widget>[
-                    Padding(
-                      padding: EdgeInsets.only(top: 20.0, bottom: 20.0),
-                      child: Text(
-                        "Mood",
-                        style: const TextStyle(
-                            fontWeight: FontWeight.bold, color: Colors.green),
-                        textScaleFactor: 4,
+                  child: Column(
+                      children: <Widget>[
+                      Padding(
+                        padding: EdgeInsets.only(top: 20.0, bottom: 20.0),
+                        child: Text(
+                         "Mood",
+                          style: const TextStyle(
+                              fontWeight: FontWeight.bold, color: Colors.green),
+                         textScaleFactor: 4,
+                        ),
                       ),
-                    ),
-                  ]),
+                     DropdownButton<Moods>(
+                       hint: Text('Please choose a mood'),
+                       value: currMood,
+                       icon: const Icon(Icons.arrow_downward),
+                       style: const TextStyle(color: Colors.green),
+                       underline: Container(
+                         height: 2,
+                         color: Colors.green,
+                       ),
+                       onChanged: (newMood) {
+                         setState(() {
+                           _MoodPage().currMood = newMood!;
+                           controllerReference.insertMoodData(currMood, [_selectedDate]);
+                         });
+                       },
+                         items: Moods.values.map((Moods mood) {
+                           return DropdownMenuItem<Moods>(
+                               value: currMood,
+                               child: Text(mood.toString()));
+                         }).toList()
+                     ),
+
+
+                  ]
+                  ),
                 ))));
   }
 }
