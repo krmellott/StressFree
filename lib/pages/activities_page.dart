e532442d75945762cd94ffb1f2ac86448d21898b
Gideon Okoroafor <okoro062@umn.edu>
2022-03-26 03:22:50 -0500

    added a drop down menu for sorting activities

--- lib/pages/activities_page.dart
+++ lib/pages/activities_page.dart
@@ -1,25 +1,34 @@
 import 'dart:collection';
 
-import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:date_picker_timeline/date_picker_timeline.dart';
 import 'package:firstapp/controller/stressFree_Controller.dart';
 import 'package:firstapp/model/stressFree_Model.dart';
 import 'package:firstapp/utils/addTask_page.dart';
 import 'package:firstapp/utils/buttons.dart';
-import 'package:firstapp/utils/units_constant.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
 
-class ActivitiesPage extends StatelessWidget {
+class ActivitiesPage extends StatefulWidget {
+  @override
+  _ActivitiesPage createState() => _ActivitiesPage();
+}
+
+class _ActivitiesPage extends State<ActivitiesPage> {
   final _subHeadingFont = TextStyle(
       fontSize: 24.0, fontWeight: FontWeight.bold, color: Colors.grey);
+  final _subHeadingFont1 = TextStyle(
+      fontSize: 24.0, fontWeight: FontWeight.bold, color: Colors.black);
   final _headingFont = TextStyle(
       fontSize: 30.0, fontWeight: FontWeight.bold, color: Colors.black);
   final controllerReference = new stressFree_Controller();
   final modelReference = new stressFree_Model();
   DateTime _selectedDate = DateTime.now();
   var date = DateTime(0, 0, 0);
+  String? selected;
+  List<String> sortBy = ['Date', 'Title', 'Priority'];
+
+  stressFree_Model model = new stressFree_Model();
 
   @override
   Widget build(BuildContext context) {
@@ -31,6 +40,7 @@ class ActivitiesPage extends StatelessWidget {
       body: Column(children: [
         _addTaskBar(context),
         _addDatePicker(),
+        _taskColumn(),
       ]),
     );
   }
@@ -133,4 +143,47 @@ class ActivitiesPage extends StatelessWidget {
       ),
     );
   }
+
+  _taskColumn(){
+    return Container(
+      margin: const EdgeInsets.only(left: 20, right: 10, top: 10),
+       child: Expanded(
+            child: SingleChildScrollView(
+                child: Column(
+                  children: <Widget>[
+                    Row(
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: <Widget>[
+                        Text('My Activities', style: _subHeadingFont1,),
+                        _insertDropDownMenu(),
+                      ],
+                    )
+                  ],
+                )
+            )
+        )
+    );
+  }
+
+  _insertDropDownMenu(){
+    return DropdownButton<String>(
+      value: selected,
+      items: sortBy.map(buildMenuItem).toList(),
+      onChanged: (value) => setState(() => selected = value),
+      icon: Icon(Icons.arrow_drop_down, color: Colors.black,),
+      hint: Text('Sort by: ', style: TextStyle(fontSize: 14, fontWeight: FontWeight.bold))
+    );
+  }
+
+  DropdownMenuItem<String> buildMenuItem(String item) => DropdownMenuItem(
+    value: item,
+      child: Text(
+        item,
+        style: TextStyle(fontWeight: FontWeight.bold, fontSize: 16),
+      )
+  );
+
+
 }
+
