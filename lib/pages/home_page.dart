b2de7281380f2bb24c741335d2a65907b0184ac2
Jacob Rohde <rohde135@d.umn.edu>
2022-03-30 17:51:42 -0500

    Added text that will contain tips to homepage, does not actually contain the tips yet.

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -19,64 +19,81 @@ class Home extends StatelessWidget {
                 title: Text("StressFree!"),
               ),
               body: Center(
-                  child: Column(
-                    children: <Widget>[
-                      Padding(
-                        padding: EdgeInsets.only(top: 20.0, bottom: 20.0),
-                        child: Text("StressFree!",style: const TextStyle(fontWeight: FontWeight.bold, color: Colors.green), textScaleFactor: 4,)
-                        ,),
+                child: Column(
+                  children: <Widget>[
+                    Expanded (
+                        child: Column (
+                          children: <Widget> [
 
-                      ElevatedButton(
-                        style: ElevatedButton.styleFrom(
-                            primary: Colors.green
-                        ),
-                        child: Text('Edit Activities'),
-                        onPressed: () {
-                          Navigator.of(context).push(MaterialPageRoute(builder: (BuildContext context) {
-                            return ActivitiesPage();
-                          }));
-                        },
-                      ),
+                            Padding(
+                              padding: EdgeInsets.only(top: 20.0, bottom: 20.0),
+                              child: Text("StressFree!",style: const TextStyle(fontWeight: FontWeight.bold, color: Colors.green), textScaleFactor: 4,),
+                            ),
 
-                      ElevatedButton(
-                        style: ElevatedButton.styleFrom(
-                            primary: Colors.green
-                        ),
-                        child: Text('Enter and View Daily Mood'),
-                        onPressed: () {
-                          Navigator.of(context).push(MaterialPageRoute(builder: (BuildContext context) {
-                            return MoodPage();
-                          }));
-                        },
-                      ),
+                            ElevatedButton(
+                              style: ElevatedButton.styleFrom(
+                                  primary: Colors.green
+                              ),
+                              child: Text('Edit Activities'),
+                              onPressed: () {
+                                Navigator.of(context).push(MaterialPageRoute(builder: (BuildContext context) {
+                                  return ActivitiesPage();
+                                }));
+                              },
+                            ),
+
+                            ElevatedButton(
+                              style: ElevatedButton.styleFrom(
+                                  primary: Colors.green
+                              ),
+                              child: Text('Enter and View Daily Mood'),
+                              onPressed: () {
+                                Navigator.of(context).push(MaterialPageRoute(builder: (BuildContext context) {
+                                  return MoodPage();
+                                }));
+                              },
+                            ),
 
 
-                      ElevatedButton(
-                          style: ElevatedButton.styleFrom(
-                              primary: Colors.green
-                          ),
-                          child: Text('View Activity Calendar and History'),
-                          onPressed: () {
-                            Navigator.of(context).push(MaterialPageRoute(builder: (BuildContext context) {
-                          return HistoryPage();
-                        }
-                        ));
-                      },
-                      ),
-                      ElevatedButton(
-                        style: ElevatedButton.styleFrom(
-                            primary: Colors.green
-                        ),
-                        child: Text('View Videos from YouTube'),
-                        onPressed: () {
-                          Navigator.of(context).push(MaterialPageRoute(builder: (BuildContext context) {
-                            return VideoPage();
-                          }
-                          ));
-                        },
-                      )
-                    ],
-                  )
+                            ElevatedButton(
+                              style: ElevatedButton.styleFrom(
+                                  primary: Colors.green
+                              ),
+                              child: Text('View Activity Calendar and History'),
+                              onPressed: () {
+                                Navigator.of(context).push(MaterialPageRoute(builder: (BuildContext context) {
+                                  return HistoryPage();
+                                }
+                                ));
+                              },
+                            ),
+
+                            ElevatedButton(
+                              style: ElevatedButton.styleFrom(
+                                  primary: Colors.green
+                              ),
+                              child: Text('View Videos from YouTube'),
+                              onPressed: () {
+                                Navigator.of(context).push(MaterialPageRoute(builder: (BuildContext context) {
+                                  return VideoPage();
+                                }
+                                ));
+                              },
+                            ),
+                          ],
+                        )
+                    ),
+
+                    Align(
+                        alignment: Alignment.bottomCenter,
+                        child: Padding(
+                            padding: EdgeInsets.only(top: 20.0, bottom: 40.0),
+                            child: Text('Tip text here', style: const TextStyle(fontWeight: FontWeight.bold, color: Colors.green), textScaleFactor: 1.5)
+                        )
+                    )
+
+                  ],
+                ),
               ),
             )
         )
