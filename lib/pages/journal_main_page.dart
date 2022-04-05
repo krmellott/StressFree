ffc7018b3bc131d51cb5a25a6b0f8fc52d307a2f
Spencer Leisch <leisc020@d.umn.edu>
2022-04-04 20:30:09 -0500

    Created journal main page and new journal entry page

--- lib/pages/journal_main_page.dart
+++ lib/pages/journal_main_page.dart
@@ -1,4 +1,5 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firstapp/utils/add_journal_entry.dart';
 import 'package:flutter/material.dart';
 
 class MainJournal extends StatefulWidget {
@@ -13,7 +14,21 @@ class _MainJournal extends State<MainJournal> {
       appBar: AppBar(
         title: Text('Journal'),
       ),
-      body: Text('These are words. They don\'t mean anything yet'),
+      body: Column (
+        children: [
+          Text('Sorry, it seems this page is still under construction. Please come back later.'
+              ' Or try out the "new Journal" button, that one should be working by now.'),
+        ]
+      ),
+        floatingActionButton: FloatingActionButton.extended(
+            onPressed: () {
+              Navigator.of(context).push(MaterialPageRoute(builder: (BuildContext context) {
+                return AddEntry();
+              }));
+              },
+            backgroundColor: Colors.green,
+            label: const Text('New Journal Entry')
+        )
     );
   }
 }
---
+++ lib/utils/add_journal_entry.dart
@@ -0,0 +1,75 @@
+import 'package:firstapp/controller/stressFree_Controller.dart';
+import 'package:firstapp/utils/buttons.dart';
+import 'package:firstapp/utils/units_constant.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class AddEntry extends StatefulWidget {
+  const AddEntry({Key? key}) : super(key: key);
+
+  @override
+  State<AddEntry> createState() => _AddEntry();
+}
+
+class _AddEntry extends State<AddEntry> {
+  stressFree_Controller SFControllerRef = stressFree_Controller();
+  DateTime date = DateTime.now();
+  String body = "";
+  String title = "";
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: const Text("New Journal Entry"),
+        ),
+        body: Column(
+          children: [
+            Row (
+              children: [
+                Expanded (
+                  child: Container(
+                    margin: const EdgeInsets.only(left: 20, right: 20, top: 10),
+                    child: TextField(
+                      decoration: InputDecoration(
+                        border: OutlineInputBorder(),
+                        labelText: 'Title your entry!',
+                      ),
+                      onChanged: (String? aTitle) {
+                        setState(() {
+                          title = aTitle!;
+                        });
+                      },
+                    ),
+                  ),
+                ),
+
+                MyButton(
+                  label: 'Save',
+                  onTap: () {
+                    SFControllerRef.insertJournalData(body, [date.month, date.day, date.year], title);
+                    Navigator.pop(context);
+                    },
+                )
+              ],
+            ),
+            Container(
+              margin: const EdgeInsets.only(left: 20, right: 10, top: 10),
+                    child: TextField(
+                      maxLines: null,
+                      decoration: InputDecoration(
+                        border: OutlineInputBorder(),
+                        labelText: 'What did you do today?',
+                      ),
+                      onChanged: (String? aBody) {
+                        setState(() {
+                          body = aBody!;
+                        });
+                      },
+                    ),
+            ),
+          ],
+        )
+    );
+  }
+}
