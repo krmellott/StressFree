1681ac926619fb887b04393e733a8e0cedc523f4
Sydney Hoglund <hoglu096@d.umn.edu>
2022-04-05 16:41:34 -0500

    added icons to buttons and started moving buttons around

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -4,7 +4,6 @@ import 'package:firstapp/pages/mood_page.dart';
 import 'package:firstapp/pages/videos_page.dart';
 import 'package:flutter/material.dart';
 import 'dart:math';
-
 import 'activities_page.dart';
 
 class Home extends StatefulWidget {
@@ -41,9 +40,34 @@ class _Home extends State<Home> {
                     textScaleFactor: 4,
                   ),
                 ),
-                ElevatedButton(
-                  style: ElevatedButton.styleFrom(primary: Colors.green),
-                  child: Text('Edit Activities'),
+                StreamBuilder (
+                    stream: FirebaseFirestore.instance.collection('tips').snapshots(),
+                    builder: (context, AsyncSnapshot<QuerySnapshot> snapshot) {
+                      return Align(
+                          alignment: Alignment.bottomCenter,
+                          child: Padding(
+                              padding: EdgeInsets.only(top: 20.0, bottom: 40.0, left: 20.0, right: 20.0),
+                              child: Text('Tip: ' + snapshot.data?.docs[tipID()]['tip'], style: const TextStyle(
+                                  fontWeight: FontWeight.bold, color: Colors.green),
+                                  textScaleFactor: 1.5)
+                          )
+                      );
+                    }
+                ),
+                Row(
+                  children:[
+                ElevatedButton.icon(
+
+                  icon: Icon(
+                    Icons.directions_run_rounded,
+                    color: Colors.white,
+                    size: 24.0,
+                  ),
+                  style: ElevatedButton.styleFrom(
+                      primary: Colors.green,
+                      shape: new RoundedRectangleBorder(borderRadius: new BorderRadius.circular(20.0)),
+                      minimumSize: Size(150, 150)),
+                  label: Text('Activities'),
                   onPressed: () {
                     Navigator.of(context).push(
                         MaterialPageRoute(builder: (BuildContext context) {
@@ -54,9 +78,17 @@ class _Home extends State<Home> {
                     });
                   },
                 ),
-                ElevatedButton(
-                  style: ElevatedButton.styleFrom(primary: Colors.green),
-                  child: Text('Enter and View Daily Mood'),
+                ElevatedButton.icon(
+                  icon: Icon(
+                    Icons.sentiment_satisfied_rounded,
+                    color: Colors.white,
+                    size: 24.0,
+                  ),
+                  style: ElevatedButton.styleFrom(
+                      primary: Colors.green,
+                      shape: new RoundedRectangleBorder(borderRadius: new BorderRadius.circular(20.0)),
+                      minimumSize: Size(150, 150)),
+                  label: Text('Mood'),
                   onPressed: () {
                     Navigator.of(context).push(
                         MaterialPageRoute(builder: (BuildContext context) {
@@ -67,9 +99,20 @@ class _Home extends State<Home> {
                     });
                   },
                 ),
-                ElevatedButton(
-                  style: ElevatedButton.styleFrom(primary: Colors.green),
-                  child: Text('View Activity Calendar and History'),
+                  ]),
+                Row(
+                  children: [
+                ElevatedButton.icon(
+                  icon: Icon(
+                    Icons.access_time_rounded,
+                    color: Colors.white,
+                    size: 24.0,
+                  ),
+                  style: ElevatedButton.styleFrom(
+                      primary: Colors.green,
+                      shape: new RoundedRectangleBorder(borderRadius: new BorderRadius.circular(20.0)),
+                      minimumSize: Size(150, 150)),
+                  label: Text('History'),
                   onPressed: () {
                     Navigator.of(context).push(
                         MaterialPageRoute(builder: (BuildContext context) {
@@ -80,9 +123,17 @@ class _Home extends State<Home> {
                     });
                   },
                 ),
-                ElevatedButton(
-                  style: ElevatedButton.styleFrom(primary: Colors.green),
-                  child: Text('View Videos from YouTube'),
+                ElevatedButton.icon(
+                  icon: Icon(
+                    Icons.play_arrow_rounded,
+                    color: Colors.white,
+                    size: 24.0,
+                  ),
+                  style: ElevatedButton.styleFrom(
+                      primary: Colors.green,
+                      shape: new RoundedRectangleBorder(borderRadius: new BorderRadius.circular(20.0)),
+                      minimumSize: Size(150, 150)),
+                  label: Text('Videos'),
                   onPressed: () {
                     Navigator.of(context).push(
                         MaterialPageRoute(builder: (BuildContext context) {
@@ -93,24 +144,10 @@ class _Home extends State<Home> {
                     });
                   },
                 ),
+                    ]),
               ],
             )
             ),
-
-            StreamBuilder (
-                stream: FirebaseFirestore.instance.collection('tips').snapshots(),
-                builder: (context, AsyncSnapshot<QuerySnapshot> snapshot) {
-                  return Align(
-                      alignment: Alignment.bottomCenter,
-                      child: Padding(
-                          padding: EdgeInsets.only(top: 20.0, bottom: 40.0, left: 20.0, right: 20.0),
-                          child: Text('Tip: ' + snapshot.data?.docs[tipID()]['tip'], style: const TextStyle(
-                              fontWeight: FontWeight.bold, color: Colors.green),
-                              textScaleFactor: 1.5)
-                      )
-                  );
-                }
-            )
           ],
         ),
       )
