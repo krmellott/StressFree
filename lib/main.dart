7b9009081bdfd20770b881cc8f91b2e5d80a2633
Jacob Rohde <rohde135@d.umn.edu>
2022-04-07 15:49:21 -0500

    Devices can now receive push notifications from database when app is closed.

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,17 @@
 import 'dart:core';
 
+import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:firstapp/pages/userSignIn_page.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 
 Future<void> main() async {
+
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
+
+  String? token = await FirebaseMessaging.instance.getToken();
+  print(token);
+
   runApp(UserAuth());
 }
