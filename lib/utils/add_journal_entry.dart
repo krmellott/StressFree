2d522beb63a782576cbf4de64996d1a909733667
Matthew Sawchuk <sawch013@d.umn.edu>
2022-04-12 16:23:36 -0500

    Made changes to the add journal entry page so that the file does not have any more errors popping up.

--- lib/utils/add_journal_entry.dart
+++ lib/utils/add_journal_entry.dart
@@ -1,7 +1,5 @@
 import 'package:firstapp/controller/stressFree_Controller.dart';
 import 'package:firstapp/utils/buttons.dart';
-import 'package:firstapp/utils/units_constant.dart';
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class AddEntry extends StatefulWidget {
@@ -12,7 +10,7 @@ class AddEntry extends StatefulWidget {
 }
 
 class _AddEntry extends State<AddEntry> {
-  stressFree_Controller SFControllerRef = stressFree_Controller();
+  StressFreeController controllerRef = StressFreeController();
   DateTime date = DateTime.now();
   String body = "";
   String title = "";
@@ -23,61 +21,52 @@ class _AddEntry extends State<AddEntry> {
         appBar: AppBar(
           title: const Text("New Journal Entry"),
         ),
-        body: Center(
-            child: Container(
-                decoration: BoxDecoration(
-                    gradient: LinearGradient(
-                        begin: Alignment.topCenter,
-                        end: Alignment.bottomCenter,
-                        colors: [Colors.lightBlueAccent, Colors.white])),
-                child: Column(
-                  children: [
-                    Row(
-                      children: [
-                        Expanded(
-                          child: Container(
-                            margin: const EdgeInsets.only(
-                                left: 20, right: 20, top: 10),
-                            child: TextField(
-                              decoration: InputDecoration(
-                                border: OutlineInputBorder(),
-                                labelText: 'Title your entry!',
-                              ),
-                              onChanged: (String? aTitle) {
-                                setState(() {
-                                  title = aTitle!;
-                                });
-                              },
-                            ),
-                          ),
-                        ),
-                        MyButton(
-                          label: 'Save',
-                          onTap: () {
-                            SFControllerRef.insertJournalData(
-                                body, [date.month, date.day, date.year], title);
-                            Navigator.pop(context);
-                          },
-                        )
-                      ],
-                    ),
-                    Container(
-                      margin:
-                          const EdgeInsets.only(left: 20, right: 10, top: 10),
-                      child: TextField(
-                        maxLines: null,
-                        decoration: InputDecoration(
-                          border: OutlineInputBorder(),
-                          labelText: 'What did you do today?',
-                        ),
-                        onChanged: (String? aBody) {
-                          setState(() {
-                            body = aBody!;
-                          });
-                        },
+        body: Column(
+          children: [
+            Row(
+              children: [
+                Expanded(
+                  child: Container(
+                    margin: const EdgeInsets.only(left: 20, right: 20, top: 10),
+                    child: TextField(
+                      decoration: InputDecoration(
+                        border: OutlineInputBorder(),
+                        labelText: 'Title your entry!',
                       ),
+                      onChanged: (String? aTitle) {
+                        setState(() {
+                          title = aTitle!;
+                        });
+                      },
                     ),
-                  ],
-                ))));
+                  ),
+                ),
+                MyButton(
+                  label: 'Save',
+                  onTap: () {
+                    controllerRef.insertJournalData(
+                        body, [date.month, date.day, date.year], title);
+                    Navigator.pop(context);
+                  },
+                )
+              ],
+            ),
+            Container(
+              margin: const EdgeInsets.only(left: 20, right: 10, top: 10),
+              child: TextField(
+                maxLines: null,
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(),
+                  labelText: 'What did you do today?',
+                ),
+                onChanged: (String? aBody) {
+                  setState(() {
+                    body = aBody!;
+                  });
+                },
+              ),
+            ),
+          ],
+        ));
   }
 }
