c7dab8b51e25b7362a5195f69867df426e7bcdf7
Gideon Okoroafor <okoro062@umn.edu>
2022-04-19 16:18:12 -0500

    cat videos screen

--- lib/pages/VideoScreens/cat_videos.dart
+++ lib/pages/VideoScreens/cat_videos.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:firstapp/utils/add_videos.dart';
 
+import '../../utils/video_player.dart';
+
 class CatVideos extends StatelessWidget {
   const CatVideos({Key? key}) : super(key: key);
 
@@ -11,7 +13,10 @@ class CatVideos extends StatelessWidget {
         title: const Text("Cute Cat Videos"),
       ),
       body: Column(
-        children: [VideoScreen(collectionPath: 'cute cat videos')],
+        children: [
+          VideoPlayer(collectionPath: 'cute cat videos'),
+          // VideoScreen(collectionPath: 'cute cat videos')
+        ],
       ),
     );
   }
