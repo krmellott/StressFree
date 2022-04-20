c97785ef11ff8d9bc38a07a718e940aeab2ac99b
Kyle Mellott <mello077@d.umn.edu>
2022-04-20 16:21:19 -0500

    Created basic sound page with sounds from local storage

--- lib/pages/sounds_page.dart
+++ lib/pages/sounds_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:audioplayers/audioplayers.dart';
 
 class SoundsPage extends StatefulWidget {
   @override
@@ -10,10 +11,165 @@ class _SoundsPage extends State<SoundsPage> {
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
+    AudioPlayer audioPlayer = AudioPlayer();
+    AudioCache audioCache = AudioCache(fixedPlayer: audioPlayer);
     return Scaffold(
         appBar: AppBar(
           title: const Text("Soothing Sounds"),
-        )
-    );
+        ),
+        body: Center(
+          child: Container(
+            decoration: BoxDecoration(
+                gradient: LinearGradient(
+                    begin: Alignment.topCenter,
+                    end: Alignment.bottomCenter,
+                    colors: [Colors.lightBlueAccent, Colors.white])),
+            child: Padding(
+              padding: EdgeInsets.only(top: 5.0, bottom: 5.0),
+              child: Container(
+                decoration: BoxDecoration(
+                    color: Colors.white
+                ),
+                child: Column(
+                  children: <Widget>[
+                    Column(
+                      children: [
+
+                        Text('Nature Sounds',
+                            style: const TextStyle(
+                                fontSize: 10, color: Colors.black),
+                            textScaleFactor: 1.5),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Padding(
+                              padding: EdgeInsets.only(left: 10.0, right: 10.0),
+                              child: IconButton(
+                                onPressed: () async {
+                                  await audioCache
+                                      .play('audio/audio/Nature_sounds.mp3');
+                                },
+                                icon: Icon(
+                                  Icons.play_arrow,
+                                ),
+                              ),
+                            ),
+                            Padding(
+                              padding: EdgeInsets.only(left: 10.0, right: 10.0),
+                              child: IconButton(
+                                onPressed: () {
+                                  audioPlayer.pause();
+                                },
+                                icon: Icon(
+                                  Icons.pause,
+                                ),
+                              ),
+                            ),
+                            Padding(
+                                padding:
+                                EdgeInsets.only(left: 10.0, right: 10.0),
+                                child: IconButton(
+                                  onPressed: () {
+                                    audioPlayer.stop();
+                                  },
+                                  icon: Icon(
+                                    Icons.stop,
+                                  ),
+                                )),
+                                ]),
+                        Text('Rain Sounds',
+                            style: const TextStyle(
+                                fontSize: 10, color: Colors.black),
+                            textScaleFactor: 1.5),
+                        Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              Padding(
+                                padding: EdgeInsets.only(left: 10.0, right: 10.0),
+                                child: IconButton(
+                                  onPressed: () async {
+                                    await audioCache
+                                        .play('audio/audio/rain_sounds.mp3');
+                                  },
+                                  icon: Icon(
+                                    Icons.play_arrow,
+                                  ),
+                                ),
+                              ),
+                              Padding(
+                                padding: EdgeInsets.only(left: 10.0, right: 10.0),
+                                child: IconButton(
+                                  onPressed: () {
+                                    audioPlayer.pause();
+                                  },
+                                  icon: Icon(
+                                    Icons.pause,
+                                  ),
+                                ),
+                              ),
+                              Padding(
+                                  padding:
+                                  EdgeInsets.only(left: 10.0, right: 10.0),
+                                  child: IconButton(
+                                    onPressed: () {
+                                      audioPlayer.stop();
+                                    },
+                                    icon: Icon(
+                                      Icons.stop,
+                                    ),
+                                  )),
+                            ]),
+                        Text('River Sounds',
+                            style: const TextStyle(
+                                fontSize: 10, color: Colors.black),
+                            textScaleFactor: 1.5),
+                        Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              Padding(
+                                padding: EdgeInsets.only(left: 10.0, right: 10.0),
+                                child: IconButton(
+                                  onPressed: () async {
+                                    await audioCache
+                                        .play('audio/audio/stream_sounds.mp3');
+                                  },
+                                  icon: Icon(
+                                    Icons.play_arrow,
+                                  ),
+                                ),
+                              ),
+                              Padding(
+                                padding: EdgeInsets.only(left: 10.0, right: 10.0),
+                                child: IconButton(
+                                  onPressed: () {
+                                    audioPlayer.pause();
+                                  },
+                                  icon: Icon(
+                                    Icons.pause,
+                                  ),
+                                ),
+                              ),
+                              Padding(
+                                  padding:
+                                  EdgeInsets.only(left: 10.0, right: 10.0),
+                                  child: IconButton(
+                                    onPressed: () {
+                                      audioPlayer.stop();
+                                    },
+                                    icon: Icon(
+                                      Icons.stop,
+                                    ),
+                                  )),
+                            ]),
+                      ],
+                        )
+                      ],
+                    ),
+
+                ),
+              ),
+            ),
+          ),
+        );
   }
 }
